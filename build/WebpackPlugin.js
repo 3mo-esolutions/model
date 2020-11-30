afedd3f65ff249a7492642c730b1e6400e539f6d
Arshia Delavar <ad@3mo.de>
2020-11-30 21:02:46 +0100

    chore: update dependencies

--- build/WebpackPlugin.js
+++ build/WebpackPlugin.js
@@ -2,6 +2,7 @@
 const CopyPlugin = require('copy-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const TerserPlugin = require('terser-webpack-plugin')
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
 
 const sharedConfigs = {
 	module: {
