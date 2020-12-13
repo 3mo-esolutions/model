21f8aabd927084a6360a8f82355140134b96f373
Arshia Delavar <ad@3mo.de>
2020-12-13 17:21:49 +0100

    feat: DialogPrompt defaultValue

--- dialogs/DialogPrompt.ts
+++ dialogs/DialogPrompt.ts
@@ -2,8 +2,8 @@ import { internalProperty, component, DialogComponent, html } from '../library'
 import { TextField } from '../components'
 
 @component('eb-dialog-input')
-export default class DialogPrompt extends DialogComponent<{ header: string, text?: string, inputLabel?: string, primaryButtonText?: string, handler: (input: string) => void }> {
-	@internalProperty() private input = ''
+export default class DialogPrompt extends DialogComponent<{ header: string, value?: string, text?: string, inputLabel?: string, primaryButtonText?: string, handler: (input: string) => void }> {
+	@internalProperty() private input = this.parameters.value ?? ''
 
 	protected render = () => html`
 		<mo-dialog
