05e0a137210e3c67d3e0566be0ebb7fdec95eafa
Arshia Delavar <ad@3mo.de>
2020-12-18 10:39:36 +0100

    feat: dynamically change gapElements of flex

--- components/Flex.ts
+++ components/Flex.ts
@@ -49,31 +49,9 @@ export default class Flex extends Component {
 	get wrap() { return this.style.flexWrap as CSS.Property.FlexWrap }
 	set wrap(value) { this.style.flexWrap = value }
 
-	private _gap?: string
-	@property()
-	get gap() { return this._gap }
-	set gap(value) {
-		this._gap = value
-		for (let i = 0; i < this.children.length; i++) {
-			const child = this.children[i] as HTMLElement
-			const marginStart = i !== 0 ? `calc(${value} / 2)` : '0'
-			const marginEnd = i !== this.children.length - 1 ? `calc(${value} / 2)` : '0'
-			switch (this.direction) {
-				case 'horizontal':
-					child.style.margin = `0 ${marginEnd} 0 ${marginStart}`
-					break
-				case 'horizontal-reversed':
-					child.style.margin = `0 ${marginStart} 0 ${marginEnd}`
-					break
-				case 'vertical':
-					child.style.margin = `${marginStart} 0 ${marginEnd} 0`
-					break
-				case 'vertical-reversed':
-					child.style.margin = `${marginEnd} 0 ${marginStart} 0`
-					break
-			}
-		}
-	}
+	@property({ type: Array, observer: applyGap }) gapElements: Array<Element> = Array.from(this.children)
+
+	@property({ observer: applyGap }) gap?: string
 
 	protected render() {
 		return html`
@@ -86,6 +64,28 @@ export default class Flex extends Component {
 			</style>
 			<slot></slot>
 		`
+	}
+}
+
+function applyGap(this: Flex) {
+	for (let i = 0; i < this.gapElements.length; i++) {
+		const child = this.gapElements[i] as HTMLElement
+		const marginStart = i !== 0 ? `calc(${this.gap} / 2)` : '0'
+		const marginEnd = i !== this.gapElements.length - 1 ? `calc(${this.gap} / 2)` : '0'
+		switch (this.direction) {
+			case 'horizontal':
+				child.style.margin = `0 ${marginEnd} 0 ${marginStart}`
+				break
+			case 'horizontal-reversed':
+				child.style.margin = `0 ${marginStart} 0 ${marginEnd}`
+				break
+			case 'vertical':
+				child.style.margin = `${marginStart} 0 ${marginEnd} 0`
+				break
+			case 'vertical-reversed':
+				child.style.margin = `${marginEnd} 0 ${marginStart} 0`
+				break
+		}
 	}
 }
 
