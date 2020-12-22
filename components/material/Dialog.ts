2fa64524298c6596367e537054a9ffa2de4b028f
Arshia Delavar <ad@3mo.de>
2020-12-22 18:13:25 +0100

    refactor: action elements in dialog via lit html

--- components/material/Dialog.ts
+++ components/material/Dialog.ts
@@ -1,6 +1,6 @@
-import { component, property, query, ComponentMixin, Snackbar } from '../../library'
+import { component, property, query, ComponentMixin, Snackbar, render, html, TemplateResult, css } from '../../library'
 import { Dialog as MwcDialog } from '@material/mwc-dialog'
-import { Button, IconButton } from '.'
+import { IconButton } from '.'
 import * as CSS from 'csstype'
 
 type ClickHandler = (() => Promise<any>) | (() => any)
@@ -28,35 +28,6 @@ export default class Dialog extends ComponentMixin(MwcDialog) {
 	primaryButtonClicked: ClickHandler = () => void 0
 	secondaryButtonClicked?: ClickHandler
 
-	constructor() {
-		super()
-
-		if (!this.primaryElement) {
-			this.btnPrimaryDefault = new Button()
-			this.btnPrimaryDefault.slot = 'primaryAction'
-			this.btnPrimaryDefault.innerText = 'OK'
-			this.appendChild(this.btnPrimaryDefault)
-		}
-
-		if (!this.secondaryElement) {
-			this.btnSecondaryDefault = new Button()
-			this.btnSecondaryDefault.slot = 'secondaryAction'
-			this.btnSecondaryDefault.innerText = 'Cancel'
-			this.appendChild(this.btnSecondaryDefault)
-		}
-
-		this.icbClose = new IconButton
-		this.icbClose.icon = 'close'
-		this.icbClose.onclick = () => this.handleCancellation()
-		this.icbClose.position = 'absolute'
-		this.icbClose.style.right = '8px'
-		this.icbClose.style.top = '8px'
-	}
-
-	private btnPrimaryDefault?: Button
-	private btnSecondaryDefault?: Button
-	private icbClose: IconButton
-
 	@property()
 	get header() { return this.heading }
 	set header(value) { this.heading = value }
@@ -66,14 +37,11 @@ export default class Dialog extends ComponentMixin(MwcDialog) {
 	set isBlocking(value) {
 		this.scrimClickAction = value ? '' : 'close'
 		this.escapeKeyAction = value ? '' : 'close'
-		this.secondaryElement.hidden = value
 		this.icbClose.hidden = value
 	}
 
-	private _size: DialogSize = 'small'
 	@property()
-	get size() { return this._size }
-	set size(value) {
+	set size(value: DialogSize) {
 		switch (value) {
 			case 'large':
 				this.style.setProperty('--mdc-dialog-max-width', 'calc(100% - 32px)')
@@ -87,40 +55,31 @@ export default class Dialog extends ComponentMixin(MwcDialog) {
 		}
 	}
 
-	@property()
-	get primaryButtonText() { return this.btnPrimaryDefault?.innerText ?? this.primaryElement.innerText }
-	set primaryButtonText(value) {
-		if (this.btnPrimaryDefault) {
-			this.btnPrimaryDefault.innerText = value
-		}
-	}
-
-	@property()
-	get secondaryButtonText() { return this.btnSecondaryDefault?.innerText ?? this.secondaryElement.innerText }
-	set secondaryButtonText(value) {
-		if (this.btnSecondaryDefault) {
-			this.secondaryElement.innerText = value
-		}
-	}
+	@property() primaryButtonText = 'OK'
+	@property() secondaryButtonText?: string
 
 	@property() actionsJustifyContent: CSS.Property.JustifyContent = 'flex-end'
 
 	protected get primaryElement() {
-		return this.querySelector('[slot="primaryAction"]') as unknown as HTMLElement
+		return this.querySelector('[slot="primaryAction"]')
 	}
 
 	protected get secondaryElement() {
-		return this.querySelector('[slot="secondaryAction"]') as unknown as HTMLElement
+		return this.querySelector('[slot="secondaryAction"]')
 	}
 
 	@query('.mdc-dialog__surface') private readonly divSurface!: HTMLDivElement
 	@query('#actions') private readonly actionsElement!: HTMLDivElement
+	@query('slot[name="primaryAction"]') private readonly primaryActionSlotElement?: HTMLSlotElement
+	@query('slot[name="secondaryAction"]') private readonly secondaryActionSlotElement?: HTMLSlotElement
 
 	protected initialized() {
-		this.divSurface.appendChild(this.icbClose)
+		this.renderCloseIconButton()
+		this.renderPrimaryButton()
+		this.renderSecondaryButton()
 		this.actionsElement.style.justifyContent = this.actionsJustifyContent
-		this.primaryElement.addEventListener('click', this.handlePrimaryButtonClick.bind(this))
-		this.secondaryElement.addEventListener('click', this.handleSecondaryButtonClick.bind(this))
+		this.primaryElement?.addEventListener('click', this.handlePrimaryButtonClick.bind(this))
+		this.secondaryElement?.addEventListener('click', this.handleSecondaryButtonClick.bind(this))
 		this.addEventListener('closed', (e: CustomEvent<{ action: 'close' | undefined }>) => {
 			if (e.detail.action === 'close') {
 				this.handleCancellation()
@@ -129,6 +88,46 @@ export default class Dialog extends ComponentMixin(MwcDialog) {
 
 		// To prevent the default behavior of executing primaryAction when Enter key is pressed
 		this.mdcRoot.onkeydown = e => e.stopImmediatePropagation()
+	}
+
+	static get styles() {
+		return css`
+			${super.styles}
+
+			#actions {
+				gap: var(--mo-thickness-xl);
+			}
+		`
+	}
+
+	protected render() {
+		this.renderPrimaryButton()
+		this.renderSecondaryButton()
+		return super.render() as TemplateResult
+	}
+
+	private icbClose = new IconButton
+	private renderCloseIconButton() {
+		this.icbClose.icon = 'close'
+		this.icbClose.onclick = () => this.handleCancellation()
+		this.icbClose.position = 'absolute'
+		this.icbClose.style.right = '8px'
+		this.icbClose.style.top = '8px'
+		this.divSurface.appendChild(this.icbClose)
+	}
+
+	private renderPrimaryButton() {
+		if (!this.primaryButtonText || !this.primaryActionSlotElement)
+			return
+
+		render(html`<mo-button raised @click=${this.handlePrimaryButtonClick.bind(this)}>${this.primaryButtonText}</mo-button>`, this.primaryActionSlotElement)
+	}
+
+	private renderSecondaryButton() {
+		if (!this.secondaryButtonText || !this.secondaryActionSlotElement)
+			return
+
+		render(html`<mo-button @click=${this.handleSecondaryButtonClick.bind(this)}>${this.secondaryButtonText}</mo-button>`, this.secondaryActionSlotElement)
 	}
 
 	private async handlePrimaryButtonClick() {
