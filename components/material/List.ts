73e88ca70ff81b48704de1cad7fd8ef779a4deab
Arshia Delavar <ad@3mo.de>
2021-02-18 14:36:33 +0100

    feat: Work with values in Lists

--- components/material/List.ts
+++ components/material/List.ts
@@ -1,5 +1,7 @@
-import { component, ComponentMixin } from '../../library'
-import { List as MwcList } from '@material/mwc-list'
+import { component, ComponentMixin, property } from '../../library'
+import { List as MwcList, MWCListIndex } from '@material/mwc-list'
+
+type ListValue = string | Array<string>
 
 /**
  * @attr activatable
@@ -12,9 +14,36 @@ import { List as MwcList } from '@material/mwc-list'
  * @attr innerRole
  * @fires action
  * @fires selected
+ * @fires change
  */
 @component('mo-list')
-export class List extends ComponentMixin(MwcList) { }
+export class List extends ComponentMixin(MwcList) {
+	@eventProperty() private readonly change!: IEvent<ListValue | undefined>
+
+	@property({ reflect: true, observer: valueChanged }) value?: ListValue
+
+	constructor() {
+		super()
+		this.addEventListener('selected', (e: CustomEvent<{ index: MWCListIndex }>) => {
+			const index = e.detail.index
+			const value = index instanceof Set
+				? this.items.filter((_, i) => index.has(i)).map(item => item?.value ?? undefined)
+				: this.items[index]?.value ?? undefined
+			this.value = value
+			this.change.trigger(value)
+		})
+	}
+}
+
+function valueChanged(this: List) {
+	if (!this.value)
+		return
+
+	const index = this.value instanceof Array
+		? new Set(this.value.map(v => this.items.findIndex(item => item.value === v)))
+		: this.items.findIndex(i => i.value === this.value)
+	this.select(index)
+}
 
 declare global {
 	interface HTMLElementTagNameMap {
