950628bd994ba57a40033dfc8c94ab9294691d40
Arshia Delavar <ad@3mo.de>
2021-02-27 13:18:01 +0100

    feat: manualClose in menu

--- components/material/Menu.ts
+++ components/material/Menu.ts
@@ -1,5 +1,8 @@
-import { component, ComponentMixin } from '../../library'
+import { component, ComponentMixin, property } from '../../library'
 import { Menu as MwcMenu } from '@material/mwc-menu'
+import { query } from 'lit-element'
+// eslint-disable-next-line import/no-internal-modules
+import { MenuSurface } from '@material/mwc-menu/mwc-menu-surface'
 
 /**
  * @attr open
@@ -28,7 +31,21 @@ import { Menu as MwcMenu } from '@material/mwc-menu'
  * @cssprop --mdc-menu-z-index
  */
 @component('mo-menu')
-export class Menu extends ComponentMixin(MwcMenu) { }
+export class Menu extends ComponentMixin(MwcMenu) {
+	@property({ type: Boolean, reflect: true, observer: manualCloseChanged }) manualClose = false
+}
+
+function manualCloseChanged(this: Menu) {
+	if (this.manualClose === false)
+		return
+
+	const surfaceElement = this.shadowRoot.querySelector('mwc-menu-surface')
+	// Here, the internals of menu and menu surface are manipulated to prevent automatic
+	// closings of the menu. Material team is aware of this problem and the issue is filed in this link:
+	// https://github.com/material-components/material-components-web-components/issues/1353
+	surfaceElement?.['deregisterBodyClick']()
+	this['mdcFoundation']['adapter'].closeSurface = () => void 0
+}
 
 declare global {
 	interface HTMLElementTagNameMap {
