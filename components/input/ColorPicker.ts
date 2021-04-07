dd2e80abcaf00836b21f2598237383ef8771c228
Arshia Delavar <ad@3mo.de>
2021-04-07 16:47:30 +0200

    presets in ColorPicker

--- components/input/ColorPicker.ts
+++ components/input/ColorPicker.ts
@@ -1,4 +1,4 @@
-import { component, html, event, property } from '../../library'
+import { component, html, event, ifDefined, nothing, property } from '../../library'
 import { Color } from '../../types'
 import { Input } from '.'
 
@@ -10,6 +10,8 @@ export class ColorPicker extends Input<Color> {
 	get value() { return this._value }
 	set value(value) { this._value = value }
 
+	@property({ type: Array }) presets?: Array<Color>
+
 	protected initialized() {
 		super.initialized()
 		this.inputElement.addEventListener<any>('input', (e: CustomEvent<undefined, HTMLInputElement>) => {
@@ -20,7 +22,34 @@ export class ColorPicker extends Input<Color> {
 
 	protected render() {
 		return html`
-			<input id='input' part='input' type='color'>
+			<style>
+				:host {
+					display: flex;
+					align-items: center;
+					justify-content: center;
+				}
+
+				input {
+					margin: 0;
+					padding: 0;
+					border: 0;
+				}
+			</style>
+			<input id='input' part='input' type='color'
+				list=${ifDefined(this.presets ? 'presetColors' : undefined)}
+				value=${ifDefined(this.value?.cssHex)}
+			>
+			${this.datalistTemplate}
+		`
+	}
+
+	private get datalistTemplate() {
+		return !this.presets ? nothing : html`
+			<datalist id='presetColors'>
+				${this.presets.map(color => html`
+					<option>${color.cssHex}</option>
+				`)}
+			</datalist>
 		`
 	}
 
