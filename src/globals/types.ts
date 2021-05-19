3f609b4fe76c8b52ec88289c28fc9c9a3f3ce521
arshia11d <arshia11d@outlook.com>
2021-05-19 22:41:48 +0200

    chore: remove unused functions

--- src/globals/types.ts
+++ src/globals/types.ts
@@ -1,20 +1,7 @@
 type AbstractConstructor<T> = abstract new (...args: Array<any>) => T
 
-function AbstractConstructor<T = unknown>(Constructor: Constructor<T>) {
-	return Constructor as AbstractConstructor<T>
-}
-
-globalThis.AbstractConstructor = AbstractConstructor
-
 type Constructor<T> = new (...args: Array<any>) => T
-
-function Constructor<T = unknown>(Constructor: Constructor<T>) {
-	return Constructor as Constructor<T>
-}
-
-globalThis.Constructor = Constructor
 
 type ParameterIndex<T extends (...args: any) => any, I extends number> = Parameters<T>[I]
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 type FirstParameter<T extends (...args: any) => any> = ParameterIndex<T, 0>
