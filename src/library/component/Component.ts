21de15d64cf37833167ff1064467cf72f0d74b51
arshia11d <arshia11d@outlook.com>
2021-05-20 12:23:06 +0200

    add support for template getter

--- src/library/component/Component.ts
+++ src/library/component/Component.ts
@@ -1,9 +1,18 @@
 import { LitElement } from 'lit-element'
 import { ComponentMixin } from '.'
+import { nothing } from '..'
 import { Observer } from './Observer'
 
 export interface ComponentConstructor extends Constructor<Component> {
 	observers: Map<PropertyKey, Observer<any>>
 }
 
-export abstract class Component extends ComponentMixin(LitElement) { }
+export abstract class Component extends ComponentMixin(LitElement) {
+	protected get template() {
+		return nothing
+	}
+
+	protected render() {
+		return this.template
+	}
+}
