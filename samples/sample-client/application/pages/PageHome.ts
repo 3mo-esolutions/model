5c202642126570b36b7908c14cd13a0b13d4a31f
arshia11d <38350963+arshia11d@users.noreply.github.com>
2021-07-23 12:42:27 +0200

    feat: MD-143: co-existing "render" method and "template" getter (#13)

--- samples/sample-client/application/pages/PageHome.ts
+++ samples/sample-client/application/pages/PageHome.ts
@@ -4,26 +4,28 @@ import { PageComponent, component, html, homePage, route, Snackbar } from '@3mo/
 @route('/home')
 @component('sample-page-home')
 export class PageHome extends PageComponent {
-	protected override render = () => html`
-		<mo-page header='Home' fullHeight>
-			<style>
-				h1 {
-					color: var(--mo-accent);
-					font-weight: 500;
-				}
-			</style>
-			<mo-card height='100%' width='100%'>
-				<mo-flex alignItems='center' justifyContent='center' height='*'>
-					<h1>Welcome to 3MO Design Library</h1>
-					<mo-flex direction='horizontal' gap='var(--mo-thickness-xl)' alignItems='center'>
-						<span>As always there is a counter in a sample project. Get used to it 😀</span>
-						<sample-counter-button
-							count='1'
-							@countChange=${(e: CustomEvent<number>) => Snackbar.show(`countChange event intercepted with the value: ${e.detail}`)}
-						></sample-counter-button>
+	protected override get template() {
+		return html`
+			<mo-page header='Home' fullHeight>
+				<style>
+					h1 {
+						color: var(--mo-accent);
+						font-weight: 500;
+					}
+				</style>
+				<mo-card height='100%' width='100%'>
+					<mo-flex alignItems='center' justifyContent='center' height='*'>
+						<h1>Welcome to 3MO Design Library</h1>
+						<mo-flex direction='horizontal' gap='var(--mo-thickness-xl)' alignItems='center'>
+							<span>As always there is a counter in a sample project. Get used to it 😀</span>
+							<sample-counter-button
+								count='1'
+								@countChange=${(e: CustomEvent<number>) => Snackbar.show(`countChange event intercepted with the value: ${e.detail}`)}
+							></sample-counter-button>
+						</mo-flex>
 					</mo-flex>
-				</mo-flex>
-			</mo-card>
-		</mo-page>
-	`
+				</mo-card>
+			</mo-page>
+		`
+	}
 }
