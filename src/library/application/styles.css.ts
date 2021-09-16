31f08df9bbe0c346ebaffe9afead4ce63a050649
Lilia Kraft <38758245+kraftlilia@users.noreply.github.com>
2021-09-16 12:38:41 +0200

    feat: transparent gray colors (#16)

--- src/library/application/styles.css.ts
+++ src/library/application/styles.css.ts
@@ -85,9 +85,15 @@ export const styles = css`
 		--mo-z-overlay: 3;
 		--mo-z-menu: 2;
 		--mo-z-surface: 1;
-		/* Themes */
+		/* Colors */
+		--mo-color-on-surface: rgba(var(--mo-color-foreground-base), 0.87) !important;
 		--mo-color-gray: rgb(var(--mo-color-gray-base)) !important;
 		--mo-color-gray-transparent: rgba(var(--mo-color-gray-base), 0.5) !important;
+		--mo-color-transparent-gray-alpha: .04;
+		--mo-color-transparent-gray-1: rgba(var(--mo-color-foreground-base), calc(var(--mo-color-transparent-gray-alpha) * 1)); /* 4% */
+		--mo-color-transparent-gray-2: rgba(var(--mo-color-foreground-base), calc(var(--mo-color-transparent-gray-alpha) * 2)); /* 8% */
+		--mo-color-transparent-gray-3: rgba(var(--mo-color-foreground-base), calc(var(--mo-color-transparent-gray-alpha) * 3)); /* 12% */
+		--mo-color-transparent-gray: var(--mo-color-transparent-gray-1);
 		/* More info: https://css-tricks.com/css-variables-calc-rgb-enforcing-high-contrast-colors/ */
 		--mo-color-accessible-base-value: calc(((((var(--mo-accent-base-r) * 299) + (var(--mo-accent-base-g) * 587) + (var(--mo-accent-base-b) * 114)) / 1000) - 128) * -1000);
 		--mo-color-accessible-base: var(--mo-color-accessible-base-value), var(--mo-color-accessible-base-value), var(--mo-color-accessible-base-value);
