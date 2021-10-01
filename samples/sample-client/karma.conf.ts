2c740e5bc5844f423489faa9faf168bc758d3e0b
Arshia Delavar <ad@3mo.de>
2021-10-01 12:03:18 +0200

    chore: debug mode for karma

--- samples/sample-client/karma.conf.ts
+++ samples/sample-client/karma.conf.ts
@@ -4,5 +4,6 @@ const karmaConfig = require('@3mo/model/build/KarmaConfig.ts')
 
 module.exports = config => {
 	karmaConfig.logLevel = config.LOG_ERROR
+	karmaConfig.debugMode = true
 	config.set(karmaConfig)
 }
