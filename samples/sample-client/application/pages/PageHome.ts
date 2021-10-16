7bba75dc5943efaded97804f20242d0a654eaf41
arshia11d <arshia11d@outlook.com>
2021-10-16 18:10:00 +0200

    update sample

--- samples/sample-client/application/pages/PageHome.ts
+++ samples/sample-client/application/pages/PageHome.ts
@@ -7,16 +7,10 @@ export class PageHome extends PageComponent {
 	protected override get template() {
 		return html`
 			<mo-page header='Home' fullHeight>
-				<style>
-					h1 {
-						color: var(--mo-accent);
-						font-weight: 500;
-					}
-				</style>
 				<mo-card height='100%' width='100%'>
-					<mo-flex alignItems='center' justifyContent='center' height='*' gap='var(--mo-thickness-xl)'>
-						<h1>Welcome to 3MO Design Library</h1>
-						<span>As always there is a counter in a sample project. Get used to it 😀</span>
+					<mo-flex height='100%' alignItems='center' justifyContent='center' gap='var(--mo-thickness-xl)'>
+						<mo-headline foreground='var(--mo-accent)'>Welcome to 3MO Design Library</mo-headline>
+						<mo-div>As always there is a counter in a sample project. Get used to it 😀</mo-div>
 						<sample-counter
 							count='1'
 							@countChange=${(e: CustomEvent<number>) => Snackbar.show(`countChange event intercepted with the value: ${e.detail}`)}
