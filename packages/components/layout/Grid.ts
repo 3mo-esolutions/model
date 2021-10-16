5b1045a678adc9b680eb4b61389667291d345cda
arshia11d <arshia11d@outlook.com>
2021-10-16 14:15:15 +0200

    better typing for grid

--- packages/components/layout/Grid.ts
+++ packages/components/layout/Grid.ts
@@ -1,14 +1,19 @@
 import { component, html, property, Component, css } from '../../library'
+import * as CSS from 'csstype'
 
 @component('mo-grid')
 export class Grid extends Component {
 	@property()
-	get rowGap() { return this.style.rowGap }
+	get rowGap() { return this.style.rowGap as CSS.Property.RowGap<string> }
 	set rowGap(value) { this.style.rowGap = value }
 
 	@property()
-	get columnGap() { return this.style.columnGap }
+	get columnGap() { return this.style.columnGap as CSS.Property.ColumnGap<string> }
 	set columnGap(value) { this.style.columnGap = value }
+
+	@property()
+	get gap() { return this.style.gap as CSS.Property.Gap<string> }
+	set gap(value) { this.style.gap = value }
 
 	@property()
 	get rows() { return this.style.gridTemplateRows.split('fr').join('*') }
@@ -19,11 +24,11 @@ export class Grid extends Component {
 	set columns(value) { this.style.gridTemplateColumns = ` ${value}`.split('*').join('fr').split(' fr').join(' 1fr').substring(1) }
 
 	@property()
-	get autoRows() { return this.style.gridAutoRows }
+	get autoRows() { return this.style.gridAutoRows as CSS.Property.GridAutoRows<string> }
 	set autoRows(value) { this.style.gridAutoRows = value }
 
 	@property()
-	get autoColumns() { return this.style.gridAutoColumns }
+	get autoColumns() { return this.style.gridAutoColumns as CSS.Property.GridAutoColumns<string> }
 	set autoColumns(value) { this.style.gridAutoColumns = value }
 
 	static override get styles() {
