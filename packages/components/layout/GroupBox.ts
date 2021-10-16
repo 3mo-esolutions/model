9e5f8da542badfaf13eee64abbc967fdf4a3b9ce
arshia11d <arshia11d@outlook.com>
2021-10-16 18:09:45 +0200

    first

--- packages/components/layout/GroupBox.ts
+++ packages/components/layout/GroupBox.ts
@@ -1,52 +1,19 @@
-import { Component, component, property, html, css } from '../../library'
-import { CSSDirection, IFlex } from '..'
-import * as CSS from 'csstype'
+import { component, html } from '../../library'
+import { Section } from './Section'
 
-/**
- * @slot
- * @slot header
- */
 @component('mo-group-box')
-export class GroupBox extends Component implements IFlex {
-	@property() header = ''
-	@property() direction: CSSDirection = 'vertical'
-	@property() wrap: CSS.Property.FlexWrap = 'unset'
-	@property() gap: CSS.Property.Gap<string> = 'unset'
-
-	static override get styles() {
-		return css`
-			:host {
-				display: block;
-			}
-
-			h3 {
-				margin: var(--mo-thickness-xl) 0 var(--mo-thickness-l) 0;
-				padding: 0 var(--mo-thickness-xs);
-				font-weight: 400;
-				transition: var(--mo-duration-quick);
-				font-size: var(--mo-font-size-l);
-				color: var(--mo-color-foreground);
-				place-self: flex-start;
-			}
-
-			h3:empty {
-				display: none;
-			}
-
-			mo-card {
-				margin: 0;
-			}
+export class GroupBox extends Section {
+	protected override get headlineTemplate() {
+		return html`
+			<mo-headline part='header' width='*' typography='headline4'>${this.header}</mo-headline>
 		`
 	}
 
-	protected override get template() {
+	protected override get contentTemplate() {
 		return html`
-			<mo-flex height='100%' width='100%'>
-				<h3 part='header'>${this.header}</h3>
-				<mo-card part='card' height='*' direction=${this.direction} wrap=${this.wrap} gap=${this.gap}>
-					<slot></slot>
-				</mo-card>
-			</mo-flex>
+			<mo-card part='card' height='*' direction=${this.direction} wrap=${this.wrap} gap=${this.gap}>
+				<slot></slot>
+			</mo-card>
 		`
 	}
 }
--- packages/components/layout/Section.ts
+++ packages/components/layout/Section.ts
@@ -12,51 +12,39 @@ export class Section extends Component implements IFlex {
 
 	static override get styles() {
 		return css`
-			:host {
-				position: relative;
-				transition: var(--mo-duration-quick);
-				background-color: transparent;
-				display: flex;
-				flex-direction: column;
-			}
-
-			:host(:empty) {
-				display: none !important;
-			}
-
-			h3 {
-				place-self: flex-start;
-				margin: var(--mo-thickness-xxl) 0 var(--mo-thickness-s) 0;
-				padding: 0 var(--mo-thickness-xs);
-				color: var(--mo-accent);
-				text-transform: uppercase;
-				font-size: var(--mo-font-size-s);
-				font-weight: 500;
-			}
-
-			h3:empty {
-				display: none !important;
-			}
-
-			mo-flex {
+			slot[name=header] {
+				display: block;
 				flex: 1;
-				align-self: stretch;
-			}
-
-			slot[name=actions] {
-				display: inline;
-				position: absolute;
-				top: 0px;
-				right: 0px;
 			}
 		`
 	}
 
 	protected override get template() {
 		return html`
-			<h3>${this.header}</h3>
-			<slot name='actions'></slot>
-			<mo-flex direction=${this.direction} gap=${this.gap} wrap=${this.wrap}>
+			<mo-flex width='100%' height='100%'>
+				<mo-flex minHeight='35px' direction='horizontal' alignItems='center'>
+					<slot name='header'>
+						${this.headlineTemplate}
+					</slot>
+					<slot name='headerAction'></slot>
+				</mo-flex>
+
+				${this.contentTemplate}
+			</mo-flex>
+		`
+	}
+
+	protected get headlineTemplate() {
+		return html`
+			<mo-headline part='header' typography='headline5' foreground='var(--mo-accent)' style='text-transform: uppercase'>
+				${this.header}
+			</mo-headline>
+		`
+	}
+
+	protected get contentTemplate() {
+		return html`
+			<mo-flex height='*' direction=${this.direction} gap=${this.gap} wrap=${this.wrap}>
 				<slot></slot>
 			</mo-flex>
 		`
