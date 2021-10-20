8516a222b6ad10c14f8caa66785daa5aa502a916
arshia11d <arshia11d@outlook.com>
2021-10-20 16:47:07 +0200

    chore: update headline font size values

--- packages/components/Headline.ts
+++ packages/components/Headline.ts
@@ -22,47 +22,47 @@ export class Headline extends Component {
 			}
 
 			:host([typography=headline1]) {
-				font-size: var(--mdc-typography-headline1-font-size, 2.5em);
+				font-size: var(--mdc-typography-headline1-font-size, min(2.5em, var(--mo-font-size-xxl)));
 				letter-spacing: -0.75px;
 				font-weight: 300;
 			}
 
 			:host([typography=headline2]) {
-				font-size: var(--mdc-typography-headline2-font-size, 2em);
+				font-size: var(--mdc-typography-headline2-font-size, min(2em, calc(calc(var(--mo-font-size-xl) + var(--mo-font-size-xxl)) / 2)));
 				letter-spacing: -0.5px;
 				font-weight: 300;
 			}
 
 			:host([typography=headline3]) {
-				font-size: var(--mdc-typography-headline3-font-size, 1.8em);
+				font-size: var(--mdc-typography-headline3-font-size, min(1.8em, var(--mo-font-size-xl)));
 				letter-spacing: -0.25px;
 				font-weight: 400;
 			}
 
 			:host([typography=headline4]) {
-				font-size: var(--mdc-typography-headline4-font-size, 1.5em);
+				font-size: var(--mdc-typography-headline4-font-size, min(1.5em, var(--mo-font-size-l)));
 				font-weight: 400;
 			}
 
 			:host([typography=headline5]) {
-				font-size: var(--mdc-typography-headline5-font-size, 1.17em);
+				font-size: var(--mdc-typography-headline5-font-size, min(1.17em, calc(calc(var(--mo-font-size-m) + var(--mo-font-size-l)) / 2)));
 				font-weight: 500;
 			}
 
 			:host([typography=headline6]) {
-				font-size: var(--mdc-typography-headline6-font-size, 1em);
+				font-size: var(--mdc-typography-headline6-font-size, min(1em, var(--mo-font-size-m)));
 				letter-spacing: 0.15px;
 				font-weight: 500;
 			}
 
 			:host([typography=subtitle1]) {
-				font-size: var(--mdc-typography-subtitle1-font-size, 1.15em);
+				font-size: var(--mdc-typography-subtitle1-font-size, min(1.15em, var(--mo-font-size-l)));
 				letter-spacing: 0.15px;
 				font-weight: 400;
 			}
 
 			:host([typography=subtitle2]) {
-				font-size: var(--mdc-typography-subtitle2-font-size, 1em);
+				font-size: var(--mdc-typography-subtitle2-font-size, min(1em, var(--mo-font-size-m)));
 				letter-spacing: 0.1px;
 				font-weight: 400;
 			}
