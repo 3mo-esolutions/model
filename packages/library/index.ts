8c48e05872c5783886f4f60e99f2718ab52d6576
arshia11d <arshia11d@outlook.com>
2021-12-29 16:23:49 +0100

    chore: export new lit 2 features

--- packages/library/index.ts
+++ packages/library/index.ts
@@ -1,7 +1,7 @@
 /* eslint-disable import/no-internal-modules */
-export { CSSResult, html, css, unsafeCSS, CSSResultArray, TemplateResult, PropertyValues } from 'lit'
+export { CSSResult, html, css, unsafeCSS, CSSResultArray, TemplateResult, PropertyValues, ReactiveController, ReactiveControllerHost, ReactiveElement } from 'lit'
 export { customElement as component, queryAsync } from 'lit/decorators.js'
-export { Directive, directive } from 'lit/directive.js'
+export * from 'lit/directive.js'
 export { render } from 'lit-html'
 export { repeat } from 'lit-html/directives/repeat.js'
 export { cache } from 'lit-html/directives/cache.js'
