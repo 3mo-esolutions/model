e5eb6122fa4a449580b95ad50827e216a820d9b4
arshia11d <arshia11d@outlook.com>
2022-01-06 13:01:03 +0100

    chore: disable warnings for all LitElements for now

--- packages/library/Component.ts
+++ packages/library/Component.ts
@@ -5,14 +5,14 @@ export interface ComponentConstructor extends Constructor<Component> {
 	observers: Map<PropertyKey, Observer<any>>
 }
 
+LitElement.enabledWarnings = []
+
 // REFACTOR: after https://github.com/microsoft/TypeScript/issues/35822 is solved,
 // both ComponentMixin and StyleMixin can be moved into library.
 // The following line would be then be `export abstract class Component extends ComponentMixin(LitElement)`
 // and the class won't need any implementation.
 
 export abstract class Component extends ComponentMixin(LitElement) {
-	static override enabledWarnings = []
-
 	protected get template() {
 		return nothing
 	}
