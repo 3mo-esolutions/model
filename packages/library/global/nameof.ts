985b98ea05345750e14eba94e0a9e4730179c313
arshia11d <arshia11d@outlook.com>
2022-03-17 20:21:35 +0100

    feat: Integrate key path syntax into nameof function

--- packages/library/global/nameof.ts
+++ packages/library/global/nameof.ts
@@ -1,5 +1,15 @@
-function nameof<T>(key: keyof T) {
-	return key
+/* eslint-disable @typescript-eslint/ban-types */
+
+type KeyPathOf<T> =
+	object extends T ? string :
+	T extends ReadonlyArray<any> ? Extract<keyof T, `${number}`> | SubKeyPathOf<T, Extract<keyof T, `${number}`>> :
+	T extends object ? Extract<keyof T, string> | SubKeyPathOf<T, Extract<keyof T, string>> :
+	never
+
+type SubKeyPathOf<T, K extends string> = K extends keyof T ? `${K}.${KeyPathOf<T[K]>}` : never
+
+function nameof<T>(keyPath: KeyPathOf<T>) {
+	return keyPath
 }
 
 window.nameof = nameof
