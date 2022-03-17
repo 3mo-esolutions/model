997ecda2d898ac66fc47819d9db6196185a5a28b
arshia11d <arshia11d@outlook.com>
2022-03-17 21:29:06 +0100

    feat: getPropertyByKeyPath & setPropertyByKeyPath functions

--- packages/library/global/nameof.ts
+++ packages/library/global/nameof.ts
@@ -12,4 +12,25 @@ function nameof<T>(keyPath: KeyPathOf<T>) {
 	return keyPath
 }
 
+type KeyPathValueOf<T, KeyPath extends string = KeyPathOf<T>> =
+	KeyPath extends keyof T ? T[KeyPath] :
+	KeyPath extends `${infer K}.${infer R}` ? K extends keyof T ? KeyPathValueOf<T[K], R> : unknown :
+	unknown
+
+function getPropertyByKeyPath<T, KeyPath extends KeyPathOf<T>>(object: T, keyPath: KeyPath): KeyPathValueOf<T, KeyPath> {
+	return keyPath.split('.').reduce((value: any, key) => value === undefined || value === null ? value : value[key], object)
+}
+
+function setPropertyByKeyPath<T, KeyPath extends KeyPathOf<T>>(object: T, keyPath: KeyPath, value: KeyPathValueOf<T, KeyPath>) {
+	const keys = keyPath.split('.')
+	const lastKey = keys[keys.length - 1]
+	const otherKeysButLast = keys.slice(0, keys.length - 1)
+	const lastObject = getPropertyByKeyPath(object, otherKeysButLast.join('.') as KeyPath) ?? object as any
+	if (lastObject !== undefined || lastObject !== null) {
+		lastObject[lastKey] = value
+	}
+}
+
 window.nameof = nameof
+window.getPropertyByKeyPath = getPropertyByKeyPath
+window.setPropertyByKeyPath = setPropertyByKeyPath
