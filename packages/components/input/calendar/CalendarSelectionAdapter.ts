573ca8db322a05ad2ef34c488c553cee511943b0
arshia11d <arshia11d@outlook.com>
2022-03-25 17:13:47 +0100

    Review

--- packages/components/input/calendar/CalendarSelectionAdapter.ts
+++ packages/components/input/calendar/CalendarSelectionAdapter.ts
@@ -3,8 +3,11 @@ import { SelectableCalendar } from './SelectableCalendar'
 
 export abstract class CalendarSelectionAdapter<T> {
 	constructor(protected readonly calendar: SelectableCalendar<T>) { }
+
 	get styles() { return css`` }
+
 	abstract getDayTemplate(day: MoDate, classInfo: ClassInfo): HTMLTemplateResult
+
 	abstract getNavigatingDate(value: T | undefined): MoDate
 
 	protected select(value: T) {
--- samples/sample-client/application/pages/PageHome.ts
+++ samples/sample-client/application/pages/PageHome.ts
@@ -1,26 +1,20 @@
-import { component, html, PageComponent, homePage, route, LocalizationHelper, state } from '@3mo/model'
-
-LocalizationHelper.language.value = 'de'
+import { component, html, PageComponent, homePage, route, Snackbar } from '@3mo/model'
 
 @homePage()
 @route('/home')
 @component('sample-page-home')
 export class PageHome extends PageComponent {
-	@state() date?: Date
 	protected override get template() {
 		return html`
 			<mo-page heading='Home' fullHeight>
 				<mo-card>
 					<mo-flex height='100%' alignItems='center' justifyContent='center' gap='var(--mo-thickness-xl)'>
-						<mo-field-date label='Date'
-							.value=${this.date}
-							@change=${(e: CustomEvent<Date>) => this.date = e.detail}
-						></mo-field-date>
-
-						<mo-field-date-range label='Date Range'
-							.shortcutReferenceDate=${new MoDate(this.date ?? new Date)}
-							@change=${({ detail: [startDate, endDate] }: CustomEvent<DateRange>) => console.log(startDate, endDate)}
-						></mo-field-date-range>
+						<mo-heading foreground='var(--mo-accent)'>Welcome to 3MO Design Library</mo-heading>
+						<mo-div>As always there is a counter in a sample project. Get used to it 😀</mo-div>
+						<sample-counter
+							count='1'
+							@countChange=${(e: CustomEvent<number>) => Snackbar.show(`countChange event intercepted with the value: ${e.detail}`)}
+						></sample-counter>
 					</mo-flex>
 				</mo-card>
 			</mo-page>
