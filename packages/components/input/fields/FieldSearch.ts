ced08481c3a43c0d27968ea85ef944941f2e6446
arshia11d <arshia11d@outlook.com>
2022-04-06 20:27:55 +0200

    fix: Fix search field dispatching incorrect input events

--- packages/components/input/fields/FieldSearch.ts
+++ packages/components/input/fields/FieldSearch.ts
@@ -1,19 +1,10 @@
-import { component, html, property, renderContainer } from '../../../library'
+import { component, html, property, renderContainer, styleMap } from '../../../library'
 import { FieldTextBase } from './FieldTextBase'
 import { FieldInputMode } from '..'
-import { Debouncer } from '../../..'
 
 @component('mo-field-search')
 export class FieldSearch extends FieldTextBase {
 	@property({ reflect: true }) override inputMode: FieldInputMode = 'search'
-	@property({ type: Number }) debounce = 500
-
-	private readonly debouncer = new Debouncer()
-
-	protected override async handleInput() {
-		await this.debouncer.debounce(this.debounce)
-		super.handleInput()
-	}
 
 	@renderContainer('slot[name="leading"]')
 	protected get searchIconTemplate() {
@@ -25,19 +16,19 @@ export class FieldSearch extends FieldTextBase {
 	@renderContainer('slot[name="trailing"]')
 	protected get clearIconTemplate() {
 		return html`
-			<mo-icon-button icon='cancel' ?hidden=${!this.value} @click=${() => this.clear()}></mo-icon-button>
+			<mo-icon-button icon='cancel'
+				style=${styleMap({ visibility: !this.value ? 'hidden' : 'visible' })}
+				@click=${() => this.clear()}
+			></mo-icon-button>
 		`
 	}
 
 	protected clear() {
-		if (!this.value) {
-			return
+		if (this.inputElement.value) {
+			this.inputElement.value = ''
+			this.handleInput()
+			this.handleChange()
 		}
-		this.value = ''
-		this.requestUpdate()
-		this.change.dispatch(this.value)
-		this.input.dispatch(this.value)
-		this.requestUpdate()
 	}
 }
 
