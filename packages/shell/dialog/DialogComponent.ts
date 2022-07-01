119b13e222be6e06a5322da2d7c7098ba9567dc0
arshia11d <arshia11d@outlook.com>
2022-07-01 12:16:13 +0200

    done

--- packages/shell/dialog/DialogComponent.ts
+++ packages/shell/dialog/DialogComponent.ts
@@ -1,5 +1,5 @@
 import { Component, PropertyValues, event, } from '../../library'
-import { Dialog } from '../..'
+import { Dialog, KeyboardHelper, WindowHelper } from '../..'
 
 export type DialogParameters = void | Record<string, any>
 
@@ -11,7 +11,36 @@ export abstract class DialogComponent<T extends DialogParameters = void, TResult
 	}
 
 	confirm(): Promise<TResult> {
+		if (KeyboardHelper.ctrlPressed || KeyboardHelper.shiftPressed || KeyboardHelper.metaPressed) {
+			return this.confirmAsPopup()
+		}
 		return MoDeL.application.dialogHost.confirm(this)
+	}
+
+	async confirmAsPopup() {
+		const Constructor = this.constructor as unknown as typeof DialogComponent
+		const propertiesToCopy = Array.from(Constructor.elementProperties.keys())
+		const popupWindow = await WindowHelper.open(window.location.pathname, { popup: true })
+		const DialogConstructor = popupWindow.customElements.get(this.tagName.toLowerCase()) as CustomElementConstructor
+		const dialogComponent = new DialogConstructor(this.parameters) as DialogComponent<T, TResult>
+		// @ts-expect-error elementProperties exists for reactive elements
+		propertiesToCopy.forEach(property => dialogComponent[property] = this[property])
+		const confirmPromise = dialogComponent.confirm()
+		await dialogComponent.updateComplete
+		if (dialogComponent.dialog) {
+			dialogComponent.dialog.boundToWindow = true
+		}
+		return confirmPromise
+	}
+
+	// eslint-disable-next-line @typescript-eslint/member-ordering
+	private minimized = false
+	protected async continueAsPopup() {
+		this.minimized = true
+		this.open = false
+		const value = await this.confirmAsPopup()
+		this.minimized = false
+		this.close(value)
 	}
 
 	protected get dialog() {
@@ -47,14 +76,18 @@ export abstract class DialogComponent<T extends DialogParameters = void, TResult
 		this.dialog.primaryAction = this.primaryButtonAction.bind(this)
 		this.dialog.secondaryAction = this.secondaryButtonAction?.bind(this)
 		this.dialog.cancellationAction = this.cancellationAction?.bind(this)
+		this.dialog.addEventListener('requestPopup', () => this.continueAsPopup())
 		this.dialog.addEventListener('closed', (e: CustomEvent<TResult>) => {
 			// Google MWC has events in some of their components
 			// which dispatch a "closed" event with "bubbles" option set to true
 			// thus reaching the DialogComponent. This is blocked here.
-			if ((e.source instanceof Dialog) === false) {
+			const eventSourceWasNotDialog = (e.source instanceof Dialog) === false
+
+			if (eventSourceWasNotDialog || this.minimized) {
 				e.stopImmediatePropagation()
 				return
 			}
+
 			this.closed.dispatch(e.detail)
 		})
 	}
