bda9a318cdb891575781a25de643cb999364ec9f
arshia11d <arshia11d@outlook.com>
2022-07-12 15:03:11 +0200

    chore: Export deactivateInert function

--- packages/shell/helpers/nonInertable.ts
+++ packages/shell/helpers/nonInertable.ts
@@ -1,4 +1,8 @@
 export const nonInertable = () => (constructor: Constructor<HTMLElement>) => {
+	deactivateInert(constructor)
+}
+
+export const deactivateInert = (constructor: Constructor<HTMLElement>) => {
 	Object.defineProperty(constructor.prototype, 'inert', {
 		enumerable: true,
 		configurable: false,
