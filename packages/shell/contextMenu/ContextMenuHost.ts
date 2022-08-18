cc9ef1d4d40e53e4d5fea0c9e4490c252385bd89
arshia11d <arshia11d@outlook.com>
2022-08-18 13:03:46 +0200

    refactor: Cleaner API for ContextMenuHost.open using overloads

--- packages/shell/contextMenu/ContextMenuHost.ts
+++ packages/shell/contextMenu/ContextMenuHost.ts
@@ -3,13 +3,23 @@ import { Corner } from '@material/mwc-menu'
 import { ContextMenu, ListItem } from '../..'
 import { nonInertable } from '..'
 
+type Coordinates = [x: number, y: number]
+type MouseEventOpenArguments = [mouseEvent: MouseEvent, template: TemplateResult]
+type CoordinatesOpenArguments = [coordinates: Coordinates, template: TemplateResult]
+type RelativeElementOpenArguments = [relativeElement: HTMLElement, corner: Corner, template: TemplateResult]
+type OpenArguments =
+	| MouseEventOpenArguments
+	| CoordinatesOpenArguments
+	| RelativeElementOpenArguments
+
 @component('mo-context-menu-host')
 @nonInertable()
 export class ContextMenuHost extends Component {
+	private static readonly lengthBuffer = 16
+
 	static get instance() { return MoDeL.application.renderRoot.querySelector('mo-context-menu-host') as ContextMenuHost }
-	static get openMenu() { return this.instance.openMenu }
-	static get openMenuOnElement() { return this.instance.openMenuOnElement }
-	static get closeMenu() { return this.instance.closeMenu }
+	static get open() { return this.instance.open }
+	static get close() { return this.instance.close }
 	static get contextMenu() { return this.instance.contextMenu }
 
 	@property({ type: Object }) menuContent?: TemplateResult
@@ -18,40 +28,57 @@ export class ContextMenuHost extends Component {
 
 	@query('mo-context-menu') private readonly contextMenu!: ContextMenu
 
-	private readonly lengthBuffer = 16
+	open = (...args: OpenArguments) => {
+		switch (true) {
+			case args[0] instanceof MouseEvent:
+				return this.openByMouseEvent(...args as MouseEventOpenArguments)
+			case args[0] instanceof HTMLElement:
+				return this.openByRelativeElement(...args as RelativeElementOpenArguments)
+			case args[0] instanceof Array:
+				return this.openByCoordinates(...args as CoordinatesOpenArguments)
+			default:
+				throw new TypeError('Invalid arguments')
+		}
 
-	openMenu = async (mouseEvent: MouseEvent, template: TemplateResult) => {
+	}
+
+	close = async () => {
+		this.menuContent = undefined
+		await this.updateComplete
+	}
+
+	private openByMouseEvent(mouseEvent: MouseEvent, template: TemplateResult) {
+		mouseEvent.preventDefault()
+		return this.openByCoordinates([mouseEvent.clientX, mouseEvent.clientY], template)
+	}
+
+	private async openByCoordinates(coordinates: Coordinates, template: TemplateResult) {
+		const [x, y] = coordinates
 		this.contextMenu.anchor = null
 		this.menuContent = template
-
 		if (this.list) {
 			this.list.style.opacity = '0'
 			await Promise.sleep(1)
 
-			const x = (mouseEvent.clientX + this.list.offsetWidth + this.lengthBuffer > window.innerWidth)
-				? window.innerWidth - this.list.offsetWidth - this.lengthBuffer
-				: mouseEvent.clientX
-			const y = (mouseEvent.clientY + this.list.offsetHeight + this.lengthBuffer > window.innerHeight)
-				? window.innerHeight - this.list.offsetHeight - this.lengthBuffer
-				: mouseEvent.clientY
+			const finalX = (x + this.list.offsetWidth + ContextMenuHost.lengthBuffer > window.innerWidth)
+				? window.innerWidth - this.list.offsetWidth - ContextMenuHost.lengthBuffer
+				: x
+			const finalY = (y + this.list.offsetHeight + ContextMenuHost.lengthBuffer > window.innerHeight)
+				? window.innerHeight - this.list.offsetHeight - ContextMenuHost.lengthBuffer
+				: y
 
-			this.list.style.left = `${x}px`
-			this.list.style.top = `${y}px`
+			this.list.style.left = `${finalX}px`
+			this.list.style.top = `${finalY}px`
 			this.list.style.opacity = '1'
 
 			await this.updateComplete
 		}
 	}
 
-	openMenuOnElement = async (element: HTMLElement, corner: Corner, template: TemplateResult) => {
+	private async openByRelativeElement(element: HTMLElement, corner: Corner, template: TemplateResult) {
 		this.contextMenu.corner = corner
 		this.contextMenu.anchor = element
 		this.menuContent = template
-		await this.updateComplete
-	}
-
-	closeMenu = async () => {
-		this.menuContent = undefined
 		await this.updateComplete
 	}
 
