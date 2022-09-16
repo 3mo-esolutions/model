35a2dc75a03b646be621d0d124d6d815c7c3ec5b
arshia11d <arshia11d@outlook.com>
2022-09-16 12:25:09 +0200

    chore: Unify data-grid toolbar padding

--- packages/components/datagrid/DataGrid.ts
+++ packages/components/datagrid/DataGrid.ts
@@ -373,7 +373,7 @@ export class DataGrid<TData, TDetailsElement extends Element | undefined = undef
 
 				--mo-data-grid-header-height: 32px;
 				--mo-data-grid-footer-min-height: 40px;
-				--mo-data-grid-toolbar-padding: var(--mo-thickness-xl) 0 var(--mo-thickness-xl) var(--mo-thickness-xl);
+				--mo-data-grid-toolbar-padding: var(--mo-thickness-xl);
 				--mo-data-grid-border: 1px solid var(--mo-color-transparent-gray-3);
 
 				/* --mo-data-grid-columns Generated in JS */
