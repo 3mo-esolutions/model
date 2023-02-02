cb9a1f47d7a6157263ee0f031be346fc1e76f5bf
arshia11d <arshia11d@outlook.com>
2023-02-02 16:11:05 +0100

    refactor: Refactor renderContainer using controllers

--- packages/library/renderContainer.ts
+++ packages/library/renderContainer.ts
@@ -1,41 +1,24 @@
 import { ReactiveElement, nothing, render } from '@a11d/lit'
-import { decorateReactiveElement } from '@a11d/lit/dist/decorateReactiveElement'
-
-export const customRendersSymbol = Symbol('customRenders')
 
 /** @deprecated */
 export const renderContainer = (containerQuery: string) => {
-	return (prototype: ReactiveElement, _property: string, descriptor: PropertyDescriptor) => {
-		decorateReactiveElement({
-			prototype,
-			constructorPropertyKey: customRendersSymbol,
-			initialValue: new Map,
-			lifecycleHooks: new Map([
-				['firstUpdated', renderAllTemplates],
-				['updated', renderAllTemplates],
-			])
-		}).set(containerQuery, descriptor)
-	}
-}
-
-function renderAllTemplates(this: ReactiveElement, customRenders: Map<string, PropertyDescriptor>) {
-	const extractTemplate = (descriptor: PropertyDescriptor) => {
-		if (typeof descriptor.get === 'function') {
-			return descriptor.get.call(this)
-		}
-
-		if (typeof descriptor.value === 'function') {
-			return descriptor.value.call(this)
-		}
-
-		return nothing
-	}
-
-	for (const [containerQuery, descriptor] of customRenders) {
-		const template = extractTemplate(descriptor)
-		const container = this.renderRoot.querySelector<HTMLElement>(containerQuery)
-		if (container) {
-			render(template, container)
-		}
+	return (prototype: ReactiveElement, _: string, descriptor: PropertyDescriptor) => {
+		const Constructor = prototype.constructor as typeof ReactiveElement
+		Constructor.addInitializer(element => {
+			element.addController({
+				async hostUpdated() {
+					await element.updateComplete
+					const container = element.renderRoot.querySelector<HTMLElement>(containerQuery)
+					if (container) {
+						const template = typeof descriptor.get === 'function'
+							? descriptor.get.call(element)
+							: typeof descriptor.value === 'function'
+								? descriptor.value.call(element)
+								: nothing
+						render(template, container)
+					}
+				}
+			})
+		})
 	}
 }
